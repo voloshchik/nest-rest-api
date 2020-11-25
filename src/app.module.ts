e0e5a814a73b9c59bb22e460871f84b0f643cf60
vladimir <lovkiy2012@gmail.com>
2020-11-25 20:31:06 +0300

    create database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    ProductsModule,
+    MongooseModule.forRoot(
+      `mongodb+srv://vladimir:TTSBNZZcLCYACkqN@cluster0.p1poj.mongodb.net/products?retryWrites=true&w=majority`,
+    ),
+  ],
 
   controllers: [AppController],
   providers: [AppService],
---
+++ src/products/schemas/product.schema.ts
@@ -0,0 +1,14 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type ProductDocument = Product & Document;
+
+@Schema()
+export class Product {
+  @Prop()
+  title: string;
+  @Prop()
+  number: number;
+}
+
+export const ProductSchema = SchemaFactory.createForClass(Product);
